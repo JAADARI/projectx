80657a4be25aa530bc183b5c105c019fc2430c80
JAADARI <78422942+JAADARI@users.noreply.github.com>
2021-04-09 10:47:27 +0200

    Update necessaire_belote.h

--- projet_belote_v1.0/necessaire_belote.h
+++ projet_belote_v1.0/necessaire_belote.h
@@ -2,8 +2,8 @@
 void new_game();
 void best_score();
 void  add_score(int score_winner,std::string winner_name);
-int dictionnaire_atout(int card);
-int dictionnaire_non_atout(int card);
+int dict_atout(int card);
+int dict_non_atout(int card);
 void distribuer(int player_random, int & cards, char& nom_joueur, int& card_west, int& card_north, int& card_east,  int& card_joueur);
 void melanger(int & tab,int taille);
-char dictionnaire(int card);
+char dict_num_to_char(int card);
